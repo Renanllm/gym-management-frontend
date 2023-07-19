31f4bedf8a534361bff247788ec79ff4dcd561e9
Renanllm <renanllm01@gmail.com>
2023-07-19 18:19:56 -0300

    new workflows

--- src/client/axios.ts
+++ src/client/axios.ts
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
+const apiBaseUrl = {
+  local: 'http://localhost:3000',
+  production: 'http://3.141.153.188:3000',
+}
+
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    import.meta.env.MODE === 'production'
+      ? apiBaseUrl.production
+      : apiBaseUrl.local,
   timeout: 1000,
 })
